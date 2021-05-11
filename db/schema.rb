8598c24878733f9a3dd9b5fe2ca5bf1be09462ac
sarahcahill <sjcahill03@gmail.com>
2021-05-11 12:27:33 +1000

    adding

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_05_11_021742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "memes", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "downloads"
+    t.decimal "price"
+    t.string "keywords"
+    t.string "string"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
